d509c4abb74dba9e3108aaf65e91a2c5c839eaff
John Harrison <johnh@quintar.ai>
2025-12-16 20:29:01 -0800

    added configure scripts for librdkafka and libsasl2

---
+++ configure_and_build_librdkafka.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Configure and build librdkafka
+#  git clone https://github.com/confluentinc/librdkafka
+
+########## BEGIN USER EDIT SECTION #############
+
+# Location of the source
+srcDir=~/code/librdkafka
+
+# Build type
+#  Debug
+#  Release
+#  MinSizeRel
+BUILD_TYPE=MinSizeRel
+
+# Minimum iOS version (if applicable)
+MIN_IOS=15.0
+
+# Minimum Android SDK version (if applicable)
+ANDROID_SDK_VERSION=32
+
+########## END USER EDIT SECTION #############
+
+if [ $# -lt 2 ]; then
+   echo " <this_script>.sh host arch prefix"
+   echo ""
+   echo " host (required):"
+   echo "   macos"
+   echo "   macoscatalyst"
+   echo "   iphoneos"
+   echo "   iphonesimulator"
+   echo "   xros"
+   echo "   xrsimulator"
+   echo "   android"
+   echo "   webasm"
+   echo ""
+   echo " arch (required):"
+   echo "   x86_64"
+   echo "   arm64"
+   echo "   any (only valid for webasm)"
+   echo ""
+   echo " prefix (default: /usr/local/{host}_{arch})"
+   exit -1
+fi
+BUILD_FOR=$1
+ARCH=$2
+INSTALL_PREFIX=$3
+if [ -z "$INSTALL_PREFIX" ]; then
+   INSTALL_PREFIX="/usr/local/${BUILD_FOR}_$ARCH"
+fi
+
+OPTIONS="-DRDKAFKA_BUILD_STATIC=ON \
+-DWITH_SSL=ON \
+-DWITH_SASL=ON \
+-DRDKAFKA_BUILD_EXAMPLES=OFF \
+-DRDKAFKA_BUILD_TESTS=OFF \
+-DWITH_PLUGINS=OFF \
+-DWITH_CURL=OFF \
+-DWITH_ZSTD=OFF \
+-DWITH_ZLIB=OFF \
+-DENABLE_LZ4_EXT=OFF \
+"
+
+# These options are critical for iOS, visionOS, and Android. For reasons unknown to me,
+# cmake completely ignores any prefix path given unless these are explicitly specified.
+#  https://stackoverflow.com/questions/65494246/cmakes-find-package-ignores-the-paths-option-when-building-for-ios
+OPTIONS="$OPTIONS \
+-DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH \
+-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=BOTH \
+-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH \
+"
+
+if [ -n "$INSTALL_PREFIX" ]; then
+   OPTIONS="$OPTIONS -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX"
+fi
+
+CLEAN_CMD="make clean"
+BUILD_CMD="make -j16"
+CONFIGURE_CMD="cmake .."
+if [ "$BUILD_FOR" = "iphoneos" ] || [ "$BUILD_FOR" = "iphonesimulator" ] || [ "$BUILD_FOR" = "macoscatalyst" ] || [ "$BUILD_FOR" = "xros" ] || [ "$BUILD_FOR" = "xrsimulator" ]; then
+   XCODE_DEV="$(xcode-select -p)"
+   if [ "$BUILD_FOR" = "iphoneos" ]; then
+      OPTIONS="$OPTIONS -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=$MIN_IOS -DCMAKE_OSX_ARCHITECTURES=$ARCH"
+   elif [ "$BUILD_FOR" = "iphonesimulator" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
+      OPTIONS="$OPTIONS -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=$MIN_IOS -DCMAKE_OSX_ARCHITECTURES=$ARCH -DCMAKE_OSX_SYSROOT=$SYSROOT"
+   elif [ "$BUILD_FOR" = "macoscatalyst" ]; then
+      echo NOT IMPLEMENTED
+   elif [ "$BUILD_FOR" = "xros" ]; then
+      OPTIONS="$OPTIONS -DCMAKE_SYSTEM_NAME=visionOS -DCMAKE_OSX_ARCHITECTURES=$ARCH"
+   elif [ "$BUILD_FOR" = "xrsimulator" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/XRSimulator.platform/Developer/SDKs/XRSimulator.sdk
+      OPTIONS="$OPTIONS -DCMAKE_SYSTEM_NAME=visionOS -DCMAKE_OSX_ARCHITECTURES=$ARCH -DCMAKE_OSX_SYSROOT=$SYSROOT"
+   fi
+elif [ "$BUILD_FOR" = "android" ]; then
+   if [ "$ARCH" = "arm64" ]; then
+      OPTIONS="$OPTIONS -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a"
+   else
+      OPTIONS="$OPTIONS -DCMAKE_ANDROID_ARCH_ABI=x86_64"
+   fi
+   OPTIONS="$OPTIONS -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=$ANDROID_SDK_VERSION"
+elif [ "$BUILD_FOR" = "webasm" ]; then
+   CONFIGURE_CMD="emcmake cmake .."
+   BUILD_CMD="emmake make -j16"
+elif [ "$BUILD_FOR" = "macos" ]; then
+   OPTIONS="$OPTIONS -DCMAKE_OSX_ARCHITECTURES=$ARCH"
+else
+   OPTIONS="$OPTIONS"
+fi
+
+# Let's begin.
+originalDir=`pwd`
+cd $srcDir
+mkdir -p build
+cd build
+
+# Start from scratch
+$CLEAN_CMD
+rm -f CMakeCache.txt
+
+# Run the configure script
+$CONFIGURE_CMD -DCMAKE_BUILD_TYPE=$BUILD_TYPE $OPTIONS
+
+# Build
+$BUILD_CMD
+sudo make install
+
+# Finally, return to the original directory
+cd $originalDir
---
+++ configure_and_build_libsasl2.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+# Configure and build librdkafka
+#  git clone https://github.com/cyrusimap/cyrus-sasl.git
+#
+# NOTES:
+#  - install automake, autoconf, libtool. Use homebrew if you don't want to build and install manually
+#  - after installing you may need to symlink libtoolize:
+#      ln -s /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
+#  - This will call `./autogen.sh` before configuring to avoid errors in the cases where the arch or build target changes.
+
+########## BEGIN USER EDIT SECTION #############
+
+# Location of the source
+srcDir=~/code/libsasl2
+
+# Build type
+#  Debug
+#  Release
+#  MinSizeRel
+BUILD_TYPE=MinSizeRel
+
+# Minimum iOS version (if applicable)
+MIN_IOS=15.0
+
+# Minimum Android SDK version (if applicable)
+ANDROID_SDK_VERSION=32
+
+########## END USER EDIT SECTION #############
+
+if [ $# -lt 2 ]; then
+   echo " <this_script>.sh host arch prefix"
+   echo ""
+   echo " host (required):"
+   echo "   macos"
+   echo "   macoscatalyst"
+   echo "   iphoneos"
+   echo "   iphonesimulator"
+   echo "   xros"
+   echo "   xrsimulator"
+   echo "   android"
+   echo "   webasm"
+   echo ""
+   echo " arch (required):"
+   echo "   x86_64"
+   echo "   arm64"
+   echo "   any (only valid for webasm)"
+   echo ""
+   echo " prefix (default: /usr/local/{host}_{arch})"
+   exit -1
+fi
+BUILD_FOR=$1
+ARCH=$2
+INSTALL_PREFIX=$3
+if [ -z "$INSTALL_PREFIX" ]; then
+   INSTALL_PREFIX="/usr/local/${BUILD_FOR}_$ARCH"
+fi
+
+OPTIONS=" \
+--enable-static \
+--disable-shared \
+--disable-sample \
+--enable-pic \
+--disable-otp \
+--without-saslauthd \
+"
+
+if [ -n "$INSTALL_PREFIX" ]; then
+   OPTIONS="$OPTIONS --prefix=$INSTALL_PREFIX"
+   export PKG_CONFIG_PATH="$INSTALL_PREFIX/lib/pkgconfig"
+fi
+
+PREBUILD_CMD="rm -f configure config.cache config.status; export NOCONFIGURE=YES; ./autogen.sh"
+BUILD_CMD="make -j"
+INSTALL_CMD="sudo make install"
+if [ "$BUILD_FOR" = "iphoneos" ] || [ "$BUILD_FOR" = "iphonesimulator" ] || [ "$BUILD_FOR" = "macoscatalyst" ] || [ "$BUILD_FOR" = "xros" ] || [ "$BUILD_FOR" = "xrsimulator" ]; then
+   XCODE_DEV="$(xcode-select -p)"
+   export DEVROOT="$XCODE_DEV/Toolchains/XcodeDefault.xctoolchain"
+   export PATH="$DEVROOT/usr/bin/:$PATH"
+   if [ "$BUILD_FOR" = "iphoneos" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+      CFLAGS="-target $ARCH-apple-ios$MIN_IOS"
+      OPTIONS="$OPTIONS --host=$ARCH-apple-ios"
+   elif [ "$BUILD_FOR" = "iphonesimulator" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
+      OPTIONS="$OPTIONS --host=$ARCH-apple-darwin"
+   elif [ "$BUILD_FOR" = "macoscatalyst" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+      CFLAGS="-target $ARCH-apple-ios15.0-macabi"
+      OPTIONS="$OPTIONS"
+   elif [ "$BUILD_FOR" = "xros" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/XROS.platform/Developer/SDKs/XROS.sdk
+      CFLAGS="-target $ARCH-apple-xros"
+      OPTIONS="$OPTIONS --host=$ARCH-apple-ios"
+   elif [ "$BUILD_FOR" = "xrsimulator" ]; then
+      SYSROOT=$XCODE_DEV/Platforms/XRSimulator.platform/Developer/SDKs/XRSimulator.sdk
+      OPTIONS="$OPTIONS --host=$ARCH-apple-darwin"
+   fi
+   CFLAGS="$CFLAGS -arch $ARCH -Os -isysroot $SYSROOT"
+   LDFLAGS="-arch $ARCH -isysroot $SYSROOT"
+   echo "Building for $BUILD_FOR on $ARCH (sysroot=$SYSROOT)"
+   OPTIONS="$OPTIONS --disable-unix-sockets"
+elif [ "$BUILD_FOR" = "android" ]; then
+   # Following guidelines from https://developer.android.com/ndk/guides/other_build_systems
+   # and https://curl.se/docs/install.html
+   unameOut="$(uname -s)"
+   case "${unameOut}" in
+      Linux*)     HOST_TAG=linux-x86_64;;
+      Darwin*)    HOST_TAG=darwin-x86_64;;
+      *)          HOST_TAG="UNKNOWN host type: ${unameOut}"
+   esac
+
+   if [ "$ARCH" = "arm64" ]; then
+         OPTIONS="$OPTIONS --host=aarch64-linux-android"
+         TOOLS_ARCH="aarch64"
+      else
+         OPTIONS="$OPTIONS --host=x86_64-linux-android"
+         TOOLS_ARCH="x86_64"
+      fi
+
+   # Assuming ANDROID_NDK is properly installed and set
+   export ANDROID_NDK_HOME=$ANDROID_NDK
+   export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG
+   export AR=$TOOLCHAIN/bin/llvm-ar
+   export AS=$TOOLCHAIN/bin/llvm-as
+   export CC=$TOOLCHAIN/bin/$TOOLS_ARCH-linux-android$ANDROID_SDK_VERSION-clang
+   export CXX=$TOOLCHAIN/bin/$TOOLS_ARCH-linux-android$ANDROID_SDK_VERSION-clang++
+   export LD=$TOOLCHAIN/bin/ld
+   export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+   export STRIP=$TOOLCHAIN/bin/llvm-strip
+
+   # This is required for some systems. BoringSSL is partially C++.
+   LDFLAGS="$LDFLAGS -lc++"
+
+   OPTIONS="$OPTIONS --with-openssl=$INSTALL_PREFIX"
+elif [ "$BUILD_FOR" = "macos" ]; then
+   CFLAGS="-target $ARCH-apple-darwin"
+   OPTIONS="$OPTIONS --host $ARCH-apple-darwin"
+elif [ "$BUILD_FOR" = "win32" ]; then
+   #OPTIONS="$OPTIONS --without-openssl --with-schannel"
+   OPTIONS="MACHINE=x64 ENABLE_SCHANNEL=yes"
+   PREBUILD_CMD="./buildconf.bat; cd winbuild"
+   BUILD_CMD="nmake /f Makefile.vc mode=static $OPTIONS"
+   INSTALL_CMD="cmake --build . --target install"
+else
+   OPTIONS="$OPTIONS"
+fi
+
+# Let's begin.
+originalDir=`pwd`
+cd $srcDir
+
+export CFLAGS="$CFLAGS"
+export LDFLAGS="$LDFLAGS"
+
+# Start from scratch
+eval $PREBUILD_CMD
+
+# Run the configure script
+./configure $OPTIONS
+
+# Build. Don't worry about errors, we don't care about curl, just libcurl
+$BUILD_CMD || true
+
+$INSTALL_CMD
+
+# Finally, return to the original directory
+cd $originalDir
